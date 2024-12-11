6a5ce15969d309107932535dc030a6bc84d86e0f
Caryen Song <caryensong@googlemai.com>
2024-12-11 15:18:40 +0100

    add new object

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-let notesTitles = [];
-let notes = [];
+let allNotes ={
+  'notesTitles' : [],
+  'notes' :[],
 
-let trashNotesTitles = [];
-let trashNotes = [];
+  'trashNotesTitles' :[],
+  'trashNotes' :[],
 
-let saveNotesTitles =[];
-let saveNotes =[];
-
+  'saveNotesTitles' :[],
+  'saveNotes' :[]
+}
 
 function init() {
   getFromLocalStorage();
@@ -19,7 +20,7 @@ function renderNotes() {
   let contentRef = document.getElementById("content");
   contentRef.innerHTML = "";
 
-  for (let indexNote = 0; indexNote < notes.length; indexNote++) {
+  for (let indexNote = 0; indexNote < allNotes.notes.length; indexNote++) {
     contentRef.innerHTML += getNoteTemplate(indexNote);
   }
 }
@@ -38,8 +39,8 @@ function addNoteAndTitle() {
     }, 3000);
     return; //wird beendet wenn feld leer ist
 }
-notesTitles.push(titleInput);
-notes.push(noteInput);
+allNotes.notesTitles.push(titleInput);
+allNotes.notes.push(noteInput);
 
 saveToLocalStorage();
 renderNotes();
@@ -53,14 +54,14 @@ noteInputRef.value = "";
 // ------------- local Storage -----------//
 
 function saveToLocalStorage() {
-  localStorage.setItem("notesTitles", JSON.stringify(notesTitles));
-  localStorage.setItem("notes", JSON.stringify(notes));
+  localStorage.setItem("notesTitles", JSON.stringify(allNotes.notesTitles));
+  localStorage.setItem("notes", JSON.stringify(allNotes.notes));
 
-  localStorage.setItem("saveNotesTitles", JSON.stringify(saveNotesTitles));
-  localStorage.setItem("saveNotes", JSON.stringify(saveNotes));
+  localStorage.setItem("saveNotesTitles", JSON.stringify(allNotes.saveNotesTitles));
+  localStorage.setItem("saveNotes", JSON.stringify(allNotes.saveNotes));
 
-  localStorage.setItem("trashNotesTitles", JSON.stringify(trashNotesTitles));
-  localStorage.setItem("trashNotes", JSON.stringify(trashNotes));
+  localStorage.setItem("trashNotesTitles", JSON.stringify(allNotes.trashNotesTitles));
+  localStorage.setItem("trashNotes", JSON.stringify(allNotes.trashNotes));
 }
 
 function getFromLocalStorage() {
@@ -68,24 +69,24 @@ function getFromLocalStorage() {
   let myArrTitle = JSON.parse(localStorage.getItem("notesTitles"));
  
   if( myArr && myArrTitle){
-     notes = myArr;
-     notesTitles = myArrTitle;
+  allNotes.notes = myArr;
+  allNotes.notesTitles = myArrTitle;
   }
 
 let mySaveArr = JSON.parse(localStorage.getItem("saveNotes"));
 let mySaveArrTitle = JSON.parse(localStorage.getItem("saveNotesTitles"));
 
 if( mySaveArr && mySaveArrTitle){
-  saveNotes = mySaveArr;
-  saveNotesTitles = mySaveArrTitle;
+  allNotes.saveNotes = mySaveArr;
+allNotes.saveNotesTitles = mySaveArrTitle;
 }
 
 let myTrashArr = JSON.parse(localStorage.getItem("trashNotes"));
 let myTrashArrTitle = JSON.parse(localStorage.getItem("trashNotesTitles"));
 
 if( myTrashArr && myTrashArrTitle){
-  trashNotes = myTrashArr;
-  trashNotesTitles = myTrashArrTitle;
+ allNotes.trashNotes = myTrashArr;
+  allNotes.trashNotesTitles = myTrashArrTitle;
 }
 }
 
@@ -96,7 +97,7 @@ function renderTrashNotes() {
   let deletAllBtn = document.getElementById("deleteAll");
   for (
     let indexTrashNote = 0;
-    indexTrashNote < trashNotes.length;
+    indexTrashNote < allNotes.trashNotes.length;
     indexTrashNote++
   ) {
     trashContentRef.innerHTML += getTrashNoteTemplate(indexTrashNote);
@@ -110,7 +111,7 @@ function renderSaveNotes() {
   saveContentRef.innerHTML = "";
   for (
     let indexSaveNote = 0;
-    indexSaveNote < saveNotes.length;
+    indexSaveNote < allNotes.saveNotes.length;
     indexSaveNote++
   ) {
     saveContentRef.innerHTML += getSaveNoteTemplate(indexSaveNote);
@@ -118,11 +119,67 @@ function renderSaveNotes() {
 }
 // // // // // // // // // // //
 
-function noteToSave(indexNote) {
-  let saveNote = notes.splice(indexNote, 1);
-  saveNotes.push(saveNote[0]);
-  let saveNoteTitle = notesTitles.splice(indexNote, 1);
-  saveNotesTitles.push(saveNoteTitle[0]);
+// function noteToSave(indexNote) {
+//   let saveNote = notes.splice(indexNote, 1);
+//   saveNotes.push(saveNote[0]);
+//   let saveNoteTitle = notesTitles.splice(indexNote, 1);
+//   saveNotesTitles.push(saveNoteTitle[0]);
+
+//   saveToLocalStorage();
+//   getFromLocalStorage() 
+//   renderNotes();
+//   renderSaveNotes();
+//   renderTrashNotes();
+// }
+
+
+// function saveNoteToTrash(indexNote) {
+//   let saveNoToTrash = saveNotes.splice(indexNote, 1);
+//   trashNotes.push(saveNoToTrash[0]);
+//   let saveTitleToTrash = saveNotesTitles.splice(indexNote ,1);
+//   trashNotesTitles.push(saveTitleToTrash[0]);
+
+//   saveToLocalStorage();
+//   getFromLocalStorage() 
+//   renderNotes();
+//   renderSaveNotes();
+//   renderTrashNotes();
+// }
+
+
+// function noteToTrash(indexNote) {
+//   let trashNote = notes.splice(indexNote, 1);
+//   trashNotes.push(trashNote[0]);
+//   let trashNoteTitle = notesTitles.splice(indexNote, 1);
+//   trashNotesTitles.push(trashNoteTitle[0]);
+
+//   saveToLocalStorage();
+//   getFromLocalStorage() 
+//   renderNotes();
+//   renderSaveNotes();
+//   renderTrashNotes();
+// }
+
+
+// function saveNoteToTop(indexNote){
+//   let saveNoteToTop = saveNotes.splice(indexNote, 1);
+//   notes.push(saveNoteToTop[0]);
+//   let saveTitleToTop = saveNotesTitles.splice(indexNote, 1);
+//   notesTitles.push(saveTitleToTop[0]);
+
+//   saveToLocalStorage();
+//   getFromLocalStorage() 
+//   renderNotes();
+//   renderSaveNotes();
+//   renderTrashNotes();
+// }
+
+
+function moveNote(indexNote, startKey, destinationKey){
+  let note = allNotes[startKey].splice(indexNote, 1);
+  allNotes[destinationKey].push(note[0]);
+  let noteTitle = allNotes[startKey + "Titles"].splice(indexNote, 1);
+  allNotes[destinationKey + "Titles"].push(noteTitle[0]);
 
   saveToLocalStorage();
   getFromLocalStorage() 
@@ -132,66 +189,25 @@ function noteToSave(indexNote) {
 }
 
 
-function saveNoteToTrash(indexNote) {
-  let saveNoToTrash = saveNotes.splice(indexNote, 1);
-  trashNotes.push(saveNoToTrash[0]);
-  let saveTitleToTrash = saveNotesTitles.splice(indexNote ,1);
-  trashNotesTitles.push(saveTitleToTrash[0]);
+// function trashNoteToTop(indexNote){
+//   let trashNoteToTop = allNotes.trashNotes.splice(indexNote, 1);
+//   allNotes.notes.push(trashNoteToTop[0]);
+//   let trashTitleToTop = trashNotesTitles.splice(indexNote, 1);
+//   allNotes.notesTitles.push(trashTitleToTop[0]);
 
-  saveToLocalStorage();
-  getFromLocalStorage() 
-  renderNotes();
-  renderSaveNotes();
-  renderTrashNotes();
-}
-
-
-function noteToTrash(indexNote) {
-  let trashNote = notes.splice(indexNote, 1);
-  trashNotes.push(trashNote[0]);
-  let trashNoteTitle = notesTitles.splice(indexNote, 1);
-  trashNotesTitles.push(trashNoteTitle[0]);
-
-  saveToLocalStorage();
-  getFromLocalStorage() 
-  renderNotes();
-  renderSaveNotes();
-  renderTrashNotes();
-}
-
-
-function saveNoteToTop(indexNote){
-  let saveNoteToTop = saveNotes.splice(indexNote, 1);
-  notes.push(saveNoteToTop[0]);
-  let saveTitleToTop = saveNotesTitles.splice(indexNote, 1);
-  notesTitles.push(saveTitleToTop[0]);
-
-  saveToLocalStorage();
-  getFromLocalStorage() 
-  renderNotes();
-  renderSaveNotes();
-  renderTrashNotes();
-}
-
-
-function trashNoteToTop(indexNote){
-  let trashNoteToTop = trashNotes.splice(indexNote, 1);
-  notes.push(trashNoteToTop[0]);
-  let trashTitleToTop = trashNotesTitles.splice(indexNote, 1);
-  notesTitles.push(trashTitleToTop[0]);
-
-  saveToLocalStorage();
-  getFromLocalStorage() 
-  renderNotes();
-  renderSaveNotes();
-  renderTrashNotes();
-}
+//   saveToLocalStorage();
+//   getFromLocalStorage() 
+//   renderNotes();
+//   renderSaveNotes();
+//   renderTrashNotes();
+// }
 
 
 function deleteNote(indexTrashNote) {
-  trashNotes.splice(indexTrashNote, 1);
-  trashNotesTitles.splice(indexTrashNote, 1);
- 
+  allNotes.trashNotes.splice(indexTrashNote, 1);
+  allNotes.trashNotesTitles.splice(indexTrashNote, 1);
+
+  renderNotes();
   saveToLocalStorage();
   renderTrashNotes();  
 }
@@ -199,10 +215,9 @@ function deleteNote(indexTrashNote) {
 
 function deleteAll(){
   let deletAllBtn = document.getElementById("deleteAll");
-
   
-  trashNotes =[];
-  trashNotesTitles =[];
+  allNotes.trashNotes =[];
+  allNotes.trashNotesTitles =[];
 
   saveToLocalStorage();
   renderNotes();
--- script/templates.js
+++ script/templates.js
@@ -1,10 +1,10 @@
 function getNoteTemplate(indexNote) {
   return ` 
   <div class="note_content">
-    <span class="title">${notesTitles[indexNote]}</span> ▹ ${notes[indexNote]}
+    <span class="title">${allNotes.notesTitles[indexNote]}</span> ▹ ${allNotes.notes[indexNote]}
       <div class="btn">
-        <button onclick= "noteToSave(${indexNote})"><img src="./assets/icon/ordner.png" alt="Archiv"></button>
-        <button onclick= "noteToTrash(${indexNote})"><img src="./assets/icon/trash_black.png" alt="trash"></button>
+        <button onclick= "moveNote(${indexNote},'notes','saveNotes')"><img src="./assets/icon/ordner.png" alt="Archiv"></button>
+        <button onclick= "moveNote(${indexNote},'notes','trashNotes')"><img src="./assets/icon/trash_black.png" alt="trash"></button>
       </div>
   </div>`;
 }
@@ -13,10 +13,10 @@ function getNoteTemplate(indexNote) {
 function getSaveNoteTemplate(indexSaveNote) {
     return ` 
     <div class="note_content">
-      <span class="title">${saveNotesTitles[indexSaveNote]}</span> ▹ ${saveNotes[indexSaveNote]} 
+      <span class="title">${allNotes.saveNotesTitles[indexSaveNote]}</span> ▹ ${allNotes.saveNotes[indexSaveNote]} 
         <div class="btn">
-          <button onclick= "saveNoteToTop(${indexSaveNote})"><img src="./assets/icon/top.png" alt="top"></button>
-          <button onclick= "saveNoteToTrash(${indexSaveNote})"><img src="./assets/icon/trash_black.png" alt="trash"></button>
+          <button onclick= "moveNote(${indexSaveNote},'saveNotes','notes')"><img src="./assets/icon/top.png" alt="top"></button>
+          <button onclick= "moveNote(${indexSaveNote},'saveNotes','trashNotes')"><img src="./assets/icon/trash_black.png" alt="trash"></button>
         </div>
     </div>`;
   }
@@ -25,9 +25,9 @@ function getSaveNoteTemplate(indexSaveNote) {
 function getTrashNoteTemplate(indexTrashNote) {
   return `
   <div class="note_content">
-    <span class="title">${trashNotesTitles[indexTrashNote]}</span> ▹ ${trashNotes[indexTrashNote]}
+    <span class="title">${allNotes.trashNotesTitles[indexTrashNote]}</span> ▹ ${allNotes.trashNotes[indexTrashNote]}
       <div class="btn">
-        <button onclick= "trashNoteToTop(${indexTrashNote})"><img src="./assets/icon/top.png" alt="top"></button>
+        <button onclick= "moveNote(${indexTrashNote},'trashNotes', 'notes');"><img src="./assets/icon/top.png" alt="top"></button>
         <button onclick= "deleteNote(${indexTrashNote})"><img src="./assets/icon/trash.png" alt="trash"></button>
       </div>
   </div>`;
